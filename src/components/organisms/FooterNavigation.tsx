44c626174c3e02bfdd82824898412d764f482822
mola1129 <ms.mola1129@gmail.com>
2020-01-28 21:53:13 +0900

    Add styles for fixed navigation.

--- src/components/organisms/FooterNavigation.tsx
+++ src/components/organisms/FooterNavigation.tsx
@@ -3,8 +3,18 @@ import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import HomeIcon from "@material-ui/icons/Home";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    position: "fixed",
+    bottom: 0,
+    width: "100%"
+  }
+}));
 
 const FooterNavigation: React.FC<{}> = () => {
+  const classes = useStyles();
   const [value, setValue] = React.useState("home");
 
   const handleChange = (
@@ -15,7 +25,11 @@ const FooterNavigation: React.FC<{}> = () => {
   };
 
   return (
-    <BottomNavigation value={value} onChange={handleChange}>
+    <BottomNavigation
+      className={classes.root}
+      value={value}
+      onChange={handleChange}
+    >
       <BottomNavigationAction label="Home" value="home" icon={<HomeIcon />} />
       <BottomNavigationAction
         label="Account"

440e81ea9dc119f8f47f4c9a5397d3073a53e6b9
mola1129 <ms.mola1129@gmail.com>
2020-02-03 02:17:06 +0900

    Add api for getting anime broadcast data.

--- backend/apiv1/views.py
+++ backend/apiv1/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import requests
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from config.settings.base import BASE_DIR
+import os
+import environ
+
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Create your views here.
+ANNICT_ACCESS_TOKEN = env('ANNICT_ACCESS_TOKEN')
+
+
+class AnimeBroadcastList(APIView):
+    def get(self, request):
+        sort_started_at = 'asc'
+        # filter_started_at_gt = ''
+        access_token = ANNICT_ACCESS_TOKEN
+        url = 'https://api.annict.com/v1/me/programs'
+        params = {'sort_started_at': sort_started_at,
+                  'access_token': access_token}
+
+        res = requests.get(url, params=params)
+        return Response(res.json(), status=res.status_code)
+
+
+anime_broadcast_list = AnimeBroadcastList.as_view()
